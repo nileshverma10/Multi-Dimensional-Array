8a8ce6b5bd1867879fcf50d17576f29c10338a0b
Nilesh Verma <100075212+nileshverma10@users.noreply.github.com>
2022-11-08 13:51:45 +0530

    Add files via upload

---
+++ problem37.js
@@ -0,0 +1,15 @@
+let arr= [1,1,1,2,3,4,5,5,5];
+let count=0;
+let min=Infinity;
+ for(i=0; i<=arr.length-1; i++){
+     if(arr[i]<min){
+         min=arr[i];
+     }
+ }
+ for(j=0; j<=arr.length-1; j++){
+        if(min==arr[j]){
+              count++;
+        };
+ }
+ console.log(min);
+ console.log(count);
