97dae86d34acb00c02db2b42000336e62aaea5b3
Nilesh Verma <100075212+nileshverma10@users.noreply.github.com>
2022-12-15 11:01:21 +0530

    Add files via upload

--- Index.jsx
+++ Index.jsx
@@ -1,193 +1,81 @@
-import React from "react";
-import { useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 import "./Index.css";
-import TTable from "./TTable";
 
-export const Index = () => {
-  const [fullname, setFullName] = useState("");
-  const [gender, setGender] = useState("");
-  const [age, setAge] = useState();
-  const [password, setPassword] = useState();
-  const [university, setUniversity] = useState("");
-  const [department, setDepartment] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState();
-
+const Index = () => {
+  const [todos, setTodos] = useState("");
   const [data, setData] = useState([]);
-
-  const handleSubmit = () => {
-    const newData = {
-      fullname,
-      gender,
-      age,
-      password,
-      university,
-      department,
-      email,
-      phone,
-    };
+  const [apiData, setApiData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const handleAdd = (event) => {
+    const newData = { todos };
     setData((prevData) => [...prevData, newData]);
-    // e.preventDefault();
+
+    setTodos("");
+    event.preventDefault();
   };
-  // if (fullname <= 5) {
-  //   alert("Enter a Valid Name");
-  // }
+
+  const handleDelete = () => {
+    setData(data.slice(0, -1));
+  };
+
+  const getData = () => {
+    setLoading(false);
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        // console.log("res", res.data);
+        setInterval(() => {
+          setApiData(res.data);
+          setLoading(true);
+        }, 3000);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log("apiData", apiData);
+
   return (
     <>
-      <div className="bg-info box1">
-        <Container>
-          {/* <img src="./image/shadow.jpg" alt="shadow" /> */}
-          <Row>
-            <Col lg={6} md={6} sm={6}>
-              STUDENT DETAILS
-              <br />
-              <label className="name1" htmlFor="name">
-                Full Name -
-              </label>
-              <input
-                className="name2"
-                type="text"
-                placeholder="Name"
-                onChange={(e) => setFullName(e.target.value)}
-              />
-              <br />
-              <label className="gender" htmlFor="gender">
-                Gender -
-              </label>
-              <input
-                className="circle"
-                type="radio"
-                name="gender"
-                value="Male"
-                id=""
-                onChange={(e) => setGender(e.target.value)}
-              />
-              <label className="gender2" htmlFor="Male">
-                Male
-              </label>
-              <input
-                className="circle"
-                type="radio"
-                name="gender"
-                value="Female"
-                id=""
-                onChange={(e) => setGender(e.target.value)}
-              />
-              <label className="gender2" htmlFor="Male">
-                Female
-              </label>
-              <input
-                className="circle"
-                type="radio"
-                name="gender"
-                value="Other"
-                onChange={(e) => setGender(e.target.value)}
-              />
-              <label className="gender2" htmlFor="other">
-                Other
-              </label>
-              <br />
-              <label className="age1" htmlFor="age">
-                Age -
-              </label>
-              <input
-                className="age2"
-                type="number"
-                placeholder="Age"
-                onChange={(e) => setAge(e.target.value)}
-              />
-              <br />
-              <label className="pass1" htmlFor="age">
-                Password -
-              </label>
-              <input
-                className="pass2"
-                type="text"
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <br />
-              <h4 className="uni">University Detalis :-</h4>
-              <br />
-              <label className="uni2" htmlFor="">
-                University
-              </label>
-              <br />
-              <select
-                className="select1"
-                name=""
-                id=""
-                onChange={(e) => setUniversity(e.target.value)}
-              >
-                <option value="">Select University</option>
-                <option value="DAVV">Davv</option>
-                <option value="Medicaps">Medicaps</option>
-                <option value="CDIPS">CDIPS</option>
-                <option value="Arbindo">Arbindo</option>
-              </select>
-              <br />
-              <label className="uni2" htmlFor="">
-                Department
-              </label>
-              <br />
-              <select
-                className="select1"
-                name=""
-                id=""
-                onChange={(e) => setDepartment(e.target.value)}
-              >
-                <option value="">Select Department</option>
-                <option value="B.sc">B.sc</option>
-                <option value="M.sc">M.sc</option>
-                <option value="BCA">BCA</option>
-                <option value="MCA">MCA</option>
-              </select>
-              <h4 className="uni">Contact Information</h4>
-              <br />
-              <label className="email1" htmlFor="email">
-                Email -
-              </label>
-              <input
-                className="email2"
-                type="Email"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <br />
-              <label className="phone1" htmlFor="email">
-                Phone -
-              </label>
-              <input
-                className="phone2"
-                type="text"
-                placeholder="Number"
-                onChange={(e) => setPhone(e.target.value)}
-              />
-              <br />
-              <input className="check" type="checkbox" name="" id="" />
-              <h5 className="agree">I Agree yo the terms & Conditions</h5>
-              <br />
-              <input className="submit" type="submit" onClick={handleSubmit} />
-            </Col>
-            <Col lg={6} md={6} sm={6}>
-              TABLE
-              <br />
-              <TTable data={data} />
-            </Col>
-          </Row>
-        </Container>
+      <div>
+        <input
+          type="text"
+          value={todos}
+          onChange={(e) => {
+            setTodos(e.target.value);
+          }}
+        />
+        <button onClick={handleAdd}>+</button>
+        <button onClick={handleDelete}>x</button>
+
+        {data &&
+          data.map((e) => {
+            return <h3>{e.todos}</h3>;
+          })}
       </div>
-      {/* {data &&
-        data.map((e) => {
-          return (
-            <h5>
-              {e.fullname} {e.gender}
-              {e.password} {e.university}{e.department}{e.email}{e.phone}
-              {e.age}
-            </h5>
-          );
-        })} */}
+      <div className="row">
+        {loading ? (
+          apiData.map((e, i) => {
+            return (
+              <div key={i} className="col-md-4">
+                <h2>{e.body}</h2>
+                <p>{e.title}</p>
+              </div>
+            );
+          })
+        ) : (
+          <h1>Loader</h1>
+        )}
+      </div>
     </>
   );
 };
+
+export default Index;
