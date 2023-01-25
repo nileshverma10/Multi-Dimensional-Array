4ad57cefc2eaac72de04438ac086bd49b1a6a5ee
Nilesh Verma <100075212+nileshverma10@users.noreply.github.com>
2023-01-25 11:10:36 +0530

    Add files via upload

---
+++ find.js
@@ -0,0 +1,33 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mongoWithNode");
+//   dbo.collection("users").findOne({}, function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
+
+// MongoClient.connect(url, function(err, db) {
+//     if (err) throw err;
+//     var dbo = db.db("mongoWithNode");
+//     dbo.collection("users").find({}).toArray(function(err, result) {
+//       if (err) throw err;
+//       console.log(result);
+//       db.close();
+//     });
+//   });
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mongoWithNode");
+    var query = { address: "S" };
+    dbo.collection("users").find(query).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+      db.close();
+    });
+  });
